4ac2ae3bc6cc490b980d779809650010a2255dbe
Priyesh Patel <priyesh@priyesh.io>
2015-02-15 19:43:41 +0000

    Unused imports

--- src/main/java/uk/ac/cam/cl/retailcategorymapper/api/routes/GetUploadRoute.java
+++ src/main/java/uk/ac/cam/cl/retailcategorymapper/api/routes/GetUploadRoute.java
@@ -3,14 +3,8 @@ package uk.ac.cam.cl.retailcategorymapper.api.routes;
 import spark.Request;
 import spark.Response;
 import uk.ac.cam.cl.retailcategorymapper.api.exceptions.NotFoundException;
-import uk.ac.cam.cl.retailcategorymapper.db.TaxonomyDb;
 import uk.ac.cam.cl.retailcategorymapper.db.UploadDb;
-import uk.ac.cam.cl.retailcategorymapper.entities.Category;
-import uk.ac.cam.cl.retailcategorymapper.entities.Taxonomy;
 import uk.ac.cam.cl.retailcategorymapper.entities.Upload;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Get metadata for a specific taxonomy.
